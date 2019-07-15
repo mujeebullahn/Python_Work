6b0774c6dacefd5cf3e1967e0e51aee5e5d2315c
Mujeebullah Noori <MNoori@spartaglobal.com>
2019-07-15 13:52:07 +0100

    final commit of exercise

---
+++ user_funny_excer.py
@@ -0,0 +1,28 @@
+#this is where i will do my exercise as filipe told
+
+#get user first name
+user_first_name = input('Please enter your name ')
+#save to var
+#
+# #get user other things - date
+user_most_imp_date = input('what date is your fav? ')
+
+#save some other else
+user_why_imp_date = input('why is that date important? ')
+
+user_funny_fact = input('give me a funny fact ')
+
+user_fav_pokemon = input('give me your fav pokemon ')
+
+print('Hy everyone ' + user_first_name + ' fav date is ' + user_most_imp_date + ' and the reason is ' +user_why_imp_date + ' and the funny fact about the user is ' + user_funny_fact + ' and ' + user_first_name + ' fav pokemon is ' + user_fav_pokemon )
+
+
+#extra task
+
+user_num = input('give me a numer please ')
+multiply_by = user_num*42
+print(multiply_by)
+#if user_num =
+
+#do some formatting
+
