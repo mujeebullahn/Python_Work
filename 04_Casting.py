1f25441bb450a7ccb1591c21f5833e53f60d45c5
Mujeebullah Noori <MNoori@spartaglobal.com>
2019-07-15 12:12:17 +0100

    casting done

---
+++ 04_Casting.py
@@ -0,0 +1,18 @@
+    #Casting is when change the data type of an object
+    #we can cast strings to int or int to strings
+
+#casting string class to integer class
+# number_string = '10'
+# print(type(number_string))
+# num_int = int(number_string)
+
+#Casting integer class to string
+print("now lets us do the same but for strings tp inegers")
+num_int2 = 11
+print(type(num_int2))
+#change it to string using str()
+number_int2_str = str(num_int2)
+print('managed to change ',type(number_int2_str))
+
+
+
