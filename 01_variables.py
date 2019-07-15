5b2ef5922eabbe9c467ea2afce40d77989b70055
Mujeebullah Noori <MNoori@spartaglobal.com>
2019-07-15 10:01:10 +0100

    variables commit

---
+++ 01_variables.py
@@ -0,0 +1,19 @@
+    #pythin is lightly typed language 
+    #variables in python
+    #its like a box - put anything on there
+    #and open the box at anytime and access it
+    #no spcaes between the name
+    #the content on the right hand side is executed first
+
+my_variable = 'Hello there'
+
+#variables with a string being printed to console
+print(my_variable)
+
+#Reassign a variable
+
+my_variable = "I'am a new content being reassigned to my_variable"
+
+print(my_variable)
+
+
