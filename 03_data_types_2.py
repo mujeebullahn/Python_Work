ef71f2431c8e55ebfd40f9ff02fb4e593d717801
Mujeebullah Noori <MNoori@spartaglobal.com>
2019-07-15 12:01:48 +0100

    data types added

---
+++ 03_data_types_2.py
@@ -0,0 +1,69 @@
+    #Numerical integer, big integer, complex numbers
+
+
+#whole numbers - integers
+my_int = 13
+print(type(my_int))
+print(type(13)) # not the same as print(type('13'))
+
+#Floats are composite numbers
+
+print(13.1)
+print(type(13.1))
+
+#Long and Complex
+#-j
+
+#Operators
+    #Add
+
+num_a = 10
+num_b = 7.3
+print(num_a + num_b)
+print(num_a - num_b)
+print(num_b*10)
+print(num_a/2)
+
+#Modolus   --> check for remainders
+print(10 % 3)
+
+#Check if even
+print(12 % 2) #=0
+print(18%3)
+
+
+#Logical Operators
+#the evaluate two sides and return a boolean
+#check if equal
+
+a = 10
+b = 13
+print(a==b)
+print(a == 10)
+
+#Check if smaller and bigger than
+print(a>b)
+print(a>9)
+print('-----')
+print(b < a)
+print(b < 15)
+
+#check bigger than or equal to
+print(a > 10)
+print(a >= 10)
+
+
+
+
+#Check if not something
+print(a != 9) #a is not 9, it is 10, it returns true
+
+#Increment
+counter = 0
+counter += 1
+print(counter)
+counter += 1
+print(counter)
+
+
+
